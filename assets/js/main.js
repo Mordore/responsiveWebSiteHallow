a0f6807928a316fe26f74560c379001a68b80e7a
Morgan levetti <69465744+Mordore@users.noreply.github.com>
2021-11-05 23:51:46 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,13 +35,13 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
-// // SWIPPER JS
-// let homeSwiper = new Swiper(".home-swiper", {
-//     spaceBetween: 30,
-//     loop: 'true',
 
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true
-//     },
-// });
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
